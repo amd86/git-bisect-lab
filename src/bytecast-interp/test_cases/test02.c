842600991988ef2adab3566642010b65dd86ff17
Aaron <amdodds@syr.edu>
2013-04-28 16:52:37 -0400

    adding test cases to branch

---
+++ src/bytecast-interp/test_cases/test02.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+int main(int argc, char **argv){
+	printf("%d",0xdeadbeefdeadbeef);
+	return 0;
+}
---
+++ src/bytecast-interp/test_cases/test02.s
@@ -0,0 +1,31 @@
+	.file	"test02.c"
+	.section	.rodata
+.LC0:
+	.string	"%d"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	movq	%rsp, %rbp
+	.cfi_offset 6, -16
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movl	$.LC0, %eax
+	movabsq	$-2401053088876216593, %rsi
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 4.4.5-8) 4.4.5"
+	.section	.note.GNU-stack,"",@progbits
